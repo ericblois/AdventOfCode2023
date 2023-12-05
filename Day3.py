8783d3a3c0f7a7ec227e02a90175f381c27739fd
eblois <42986376+ericblois@users.noreply.github.com>
2023-12-05 02:23:47 -0500

    Simplify part 1 day 3

--- Day3.py
+++ Day3.py
@@ -1,47 +1,34 @@
 import time
 import re
 input = open("Day3Input.txt", "r")
-''''
+
 # Part 1
 start = time.time()
-for _ in range(1):
+for _ in range(100):
     input.seek(0)
-    prev = input.readline()
-    line = input.readline()
-    next = input.readline()
+    lines = input.readlines()
     total = 0
-    # First line
-    nums = [[match.start(), match.end(), int(match.group(0))] for match in re.finditer(r'(\d+)', prev)]
-    syms = [line[max(match[0]-1, 0):min(match[1]+1, len(line))] + prev[max(match[0]-1, 0)] + prev[match[1]] for match in nums]
-    for i, num in enumerate(nums):
-        if re.search(r'([^\d\.\n])', syms[i]) is None:
-            continue
-        else:
-            total += num[2]
-    # Middle lines
-    while next:
-        nums = [[match.start(), match.end(), int(match.group(0))] for match in re.finditer(r'(\d+)', line)]
-        syms = [prev[max(match[0]-1, 0):min(match[1]+1, len(prev))] + next[max(match[0]-1, 0):min(match[1]+1, len(next))] + line[max(match[0]-1, 0)] + line[match[1]] for match in nums]
-        for i, num in enumerate(nums):
-            if re.search(r'([^\d\.\n]+)', syms[i]) is None:
+    for i, line in enumerate(lines):
+        prev = lines[i-1] if i > 0 else ''
+        next = lines[i+1] if i < len(lines)-1 else ''
+        matches = [[match.start(), match.end(), int(match.group(0))] for match in re.finditer(r'(\d+)', line)]
+        syms = [
+            prev[max(match[0]-1, 0):min(match[1]+1, len(prev))]
+             + next[max(match[0]-1, 0):min(match[1]+1, len(next))]
+             + line[max(match[0]-1, 0)]
+             + line[min(match[1],len(line)-1)]
+             for match in matches
+        ]
+        for i, match in enumerate(matches):
+            if re.search(r'[^\d\.\n]+', syms[i]) is None:
                 continue
             else:
-                total += num[2]
-        prev = line
-        line = next
-        next = input.readline()
-    # Last line
-    nums = [[match.start(), match.end(), int(match.group(0))] for match in re.finditer(r'(\d+)', line)]
-    syms = [prev[max(match[0]-1, 0):min(match[1]+1, len(prev))] + line[max(match[0]-1, 0)] + line[match[1]] for match in nums]
-    for i, num in enumerate(nums):
-        if re.search(r'([^\d\.\n])', syms[i]) is None:
-            continue
-        else:
-            total += num[2]
+                total += match[2]
 end = time.time()
 print(end - start)
-print(total)'''
+print(total)
 
+# Part 2
 def put_gear(gears, pos, part):
     if pos in gears:
         gears[pos].append(part)
@@ -72,7 +59,6 @@ def find_gear(gears, match, prev, line, next, line_num):
         pos = (r_bound - 1, line_num)
         put_gear(gears, pos, int(match[2]))
 
-# Part 2
 start = time.time()
 for _ in range(100):
     input.seek(0)
